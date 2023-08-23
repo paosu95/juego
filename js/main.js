7115c25a10ef4afc86bfa55318d29b45b3b20be2
Gina Suarez <ginin_95@hotmail.com>
2023-08-22 22:01:08 -0500

    codigo del juego

---
+++ js/main.js
@@ -0,0 +1,65 @@
+const piedraOpcion = document.getElementById("piedra");
+const papelOpcion = document.getElementById("papel");
+const tijeraOpcion = document.getElementById("tijera");
+
+
+/*leemos la entrada del resultado */
+
+const resultadoJuego = document.getElementById("resultado");
+
+//iniciamos el juego
+piedraOpcion.addEventListener("click", () =>{
+    iniciarJuego('piedra');
+});
+
+papelOpcion.addEventListener("click", () =>{
+    iniciarJuego('papel');
+});
+
+tijeraOpcion.addEventListener("click", () =>{
+    iniciarJuego('tijera');
+});
+
+function iniciarJuego(opcion){
+    //movimiento pc
+    const PC = movimientoPc();
+    //movimiento usuario
+    const Usuario = opcion;
+    //comparacion de movimiento
+    const comp = comparacion(PC, Usuario);
+    //resultado
+    if (comp ==1) {
+        resultadoJuego.innerHTML = " Usuario elige "+Usuario + "<br> Pc elige "+ PC+ "<br> <span class='ganador'>El ganador es usted :) <br></span>";
+    }
+    if (comp ==2) {
+        resultadoJuego.innerHTML = " Usuario elige "+Usuario + "<br> Pc elige "+ PC+ "<br> <span class='perdedor'>El perdedor es usted :(</span>";
+    }
+    if (comp ==3) {
+        resultadoJuego.innerHTML = " Usuario elige "+Usuario + "<br> Pc elige "+ PC+ "<br> <span class='empate'>La partida es un empate </span>";
+    }
+}
+
+
+function movimientoPc(){
+    const opciones = ['piedra', 'papel', 'tijera'];
+    const random = Math.floor(Math.random()*3);
+    const mov = opciones[random];
+    return (mov);
+}
+
+function comparacion(pc, usuario){
+    switch (usuario+pc){
+        case 'piedratijera':
+        case 'papelpiedra':
+        case 'tijerapapel':
+            return 1; //gana
+        case 'tijerapiedra':
+        case 'piedrapapel':
+        case 'papeltijera':
+            return 2; //pierde
+        case 'papelpapel':
+        case 'piedrapiedra':
+        case 'tijeratijera':
+            return 3; //empata
+    }
+}
